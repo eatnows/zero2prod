a7fc55c0aeef9f3b7de9c41870033d2e4a500bcd
eatnows <gagyasindev@gmail.com>
2024-04-13 10:52:44 +0900

    feat: subscriptions api 로직 추가
    - 데이터베이스 연결해서 데이터 insert
    - 테스트 코드에서 테스트 케이스마다 논리 데이터베이스를 생성하여 테스트 독립성 확보

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,7 @@
 use actix_web::{HttpResponse, web};
+use sqlx::PgPool;
+use sqlx::types::chrono::Utc;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -6,6 +9,33 @@ pub struct FormData {
     name: String
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    // 애플리케이션 상태에서 커넥션을 꺼낸다.
+    pool: web::Data<PgPool>
+) -> HttpResponse {
+    // `Result` 는 `OK` 와 `Err` 라는 두 개의 변형 (variant)을 갖는다.
+    // 첫 번째는 성공, 두 번째는 실패를 의미한다.
+    // `match` 구문을 사용해서 결과에 따라 무엇을 수행할지 선택한다.
+    // `Result`에 관해서는 차차 자세히 설명한다!
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+        // `get_ref` 를 사용해서 `web::Data`로 감싸진 `PgConnection`에 대한 불변 참조 (immutable reference)를 얻는다.
+        .execute(pool.get_ref())
+        .await
+        {
+            Ok(_) => HttpResponse::Ok().finish(),
+            Err(e) => {
+                println!("Failed to execute query: {}", e);
+                HttpResponse::InternalServerError().finish()
+            }
+        }
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,12 @@
 // tokio::test 는 테스팅에 있어서 `tokio::main` 과 동등하다.
 // #[test] 속성을 지정하는 수고를 덜 수 있다.
 
+use std::fmt::format;
 use std::net::TcpListener;
-use sqlx::{Connection, PgConnection};
-use zero2prod::configuration::get_configuration;
+use chrono::format;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use uuid::Uuid;
+use zero2prod::configuration::{DatabaseSettings, get_configuration};
 use zero2prod::startup::run;
 
 // cargo expand --test health_check (<- 테스트 파일의 이름) 을 사용해서
@@ -11,7 +14,7 @@ use zero2prod::startup::run;
 #[tokio::test]
 async fn health_check_works() {
     // Arrange (준비)
-    let address = spawn_app();
+    let address = spawn_app().await.address;
     // `reqwest` 를 사용해서 애플리케이션에 대한 HTTP 요청을 수행한다.
     let client = reqwest::Client::new();
 
@@ -27,37 +30,75 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+async fn spawn_app() -> TestApp {
     // OS에서 0번 포트는 특별하다.
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
 
     // OS가 할당한 포트 번호를 추출한다.
     let port = listener.local_addr().unwrap().port();
-    let server = run(listener).expect("Failed to bind address");
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = format!("newsletter_{}", Uuid::new_v4().to_string());
+
+    let connection_pool = configure_database(&configuration.database).await;
+
+    // let connection_pool = PgPool::connect(
+    //     &configuration.database.connection_string()
+    // )
+    //     .await
+    //     .expect("Failed to connect to Postgres");
+
+    let server = run(listener, connection_pool.clone())
+        .expect("Failed to bind address");
     let _ = tokio::spawn(server);
-    // 애플리케이션 주소를 호출자에게 반환한다.
-    format!("http://127.0.0.1:{}", port)
+    TestApp {
+        address,
+        db_pool: connection_pool,
+    }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // 데이터베이스를 생성한다.
+    let mut connection = PgConnection::connect(
+        &config.connection_string_without_db()
+    )
+        .await
+        .expect("Failed to connect to Postgres.");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    // 데이터베이스를 마이그레이션한다.
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    // sqlx-cli 에서 sqlx migrate run 을 실행할 때 사용한 것과 동일한 매크로이다.
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
 }
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
-    let app_address = spawn_app();
-    let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_string = configuration.database.connection_string();
-    // `Connection` 트레이트는 반드시 스코프 안에 있어야 `PgConnection::connect`를 호출할 수 있다. 구조체의 상속 메서드가 아니다.
-    let mut connection = PgConnection::connect
-        (&connection_string)
-        .await
-        .expect("Failed to connect to Postgres.");
-
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .post(&format!("{}/subscriptions", &app_address))
+        .post(&format!("{}/subscriptions", &app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -68,7 +109,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-        .fetch_one(&mut connection)
+        .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription");
 
@@ -79,7 +120,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
     // Arrange
-    let app_address = spawn_app();
+    let app_address = spawn_app().await.address;
     let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
